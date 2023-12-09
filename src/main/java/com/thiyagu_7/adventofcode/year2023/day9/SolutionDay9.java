6c8e269f96072af59c809e07ff185fe55040fc93
Thiyagarajan B <38608518+thiyagu-7@users.noreply.github.com>
2023-12-09 17:44:08 +0530

    2023 - Day 9

---
+++ src/main/java/com/thiyagu_7/adventofcode/year2023/day9/SolutionDay9.java
@@ -0,0 +1,58 @@
+package com.thiyagu_7.adventofcode.year2023.day9;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.function.BiFunction;
+
+public class SolutionDay9 {
+    public int part1(List<String> input) {
+        return process(input, (diff, extrapolatedValue) -> diff[diff.length - 1] + extrapolatedValue);
+    }
+
+    public int part2(List<String> input) {
+        return process(input, (diff, extrapolatedValue) -> diff[0] - extrapolatedValue);
+    }
+
+    private int process(List<String> input, BiFunction<int[], Integer, Integer> extrapolatedValueFunc) {
+        List<int[]> list = parseInput(input);
+        int sumOfExtrapolatedValues = 0;
+        for (int[] history : list) {
+            int[] diff;
+            int[] current = history;
+            Stack<int[]> diffsStack = new Stack<>();
+            diffsStack.push(current);
+            do {
+                diff = new int[current.length - 1];
+                for (int i = 0; i < current.length - 1; i++) {
+                    diff[i] = current[i + 1] - current[i];
+                }
+                diffsStack.push(diff);
+                current = diff;
+            } while (!areAllElementsZero(current));
+
+            int extrapolatedValue = 0;
+            diffsStack.pop(); //pop off the last row with all 0's
+            while (!diffsStack.isEmpty()) {
+                diff = diffsStack.pop();
+                extrapolatedValue = extrapolatedValueFunc.apply(diff, extrapolatedValue);//diff[0] - extrapolatedValue;
+            }
+            sumOfExtrapolatedValues += extrapolatedValue;
+        }
+        return sumOfExtrapolatedValues;
+    }
+
+    private boolean areAllElementsZero(int[] arr) {
+        return Arrays.stream(arr)
+                .allMatch(e -> e == 0);
+    }
+
+    private List<int[]> parseInput(List<String> input) {
+        return input.stream()
+                .map(line -> line.split(" "))
+                .map(strArr -> Arrays.stream(strArr)
+                        .mapToInt(Integer::parseInt)
+                        .toArray())
+                .toList();
+    }
+}
